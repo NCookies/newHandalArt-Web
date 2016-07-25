7bb9dc7a85e18134b1da15974fa1c429a862a9d4
unknown <>
2016-07-25 22:55:17 +0900

    tdEdit process

--- public/js/mandal.js
+++ public/js/mandal.js
@@ -1,8 +1,41 @@
+$( function() {
+  $( "#datepicker" ).datepicker({
+      dateFormat: 'yy-mm-dd', // 데이터는 yyyy-MM-dd로 나옴
+      closeText: '닫기',
+      prevText: '이전달',
+      nextText: '다음달',
+      currentText: '오늘',
+      monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+      monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+      dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+      dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+      dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+      changeMonth: true,
+      changeYear: true,
+      minDate: -20
+  });
+});
+
+function tdEdit(e) {
+  e.stopPropagation();
+  var currentEle = $(this);
+  $(this).empty();
+  var value = $(this).html();
+    
+  $(currentEle).html('<input class="thVal" type="text" value="' + value + '" />');
+  //$(".thVal").focus();
+  $(".thVal").keyup(function (event) {
+    if (event.keyCode == 13) {
+      $(currentEle).html($(".thVal").val().trim());
+    }
+  });
+}
+
 function funLoad() {
   // 창 크기에 맞춰서 사이즈 조정
-	var Cheight = $(window).height()-500;
-	alert(Cheight);
-	$('.mandal-view-section').css({'height':Cheight+'px'});
+  var Cheight = $(window).height()-500;
+  alert(Cheight);
+  $('.mandal-view-section').css({'height':Cheight+'px'});
   $('.mandal-mandal-section').css({'height':Cheight+'px'});
   //window.onload = funLoad;
   //window.onresize = funLoad;  
@@ -35,32 +68,14 @@ function checkKey(e) {
   }
 }
 
-$( function() {
-  $( "#datepicker" ).datepicker({
-      dateFormat: 'yy-mm-dd', // 데이터는 yyyy-MM-dd로 나옴
-      closeText: '닫기',
-      prevText: '이전달',
-      nextText: '다음달',
-      currentText: '오늘',
-      monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-      monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-      dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-      dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-      dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-      changeMonth: true,
-      changeYear: true,
-      minDate: -20
-  });
-});
-
 $('article').on({
   'click': activate,
-  'blur': dismiss,
+  'blur': dismiss
+});
+
+$('td').on({
+  'click': tdEdit,
   'keyup': checkKey
 });
 
 $('.dismiss').on('click', dismiss);
-
-$('td').on('click', function() {
-  console.log("td click");
-});
