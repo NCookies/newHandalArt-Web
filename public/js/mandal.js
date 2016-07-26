89aaf10602a009a974890fdde44bd5ff09b6dd3d
leesh054 <leesh054>
2016-07-26 17:00:17 +0900

    modifined setting form

--- public/js/mandal.js
+++ public/js/mandal.js
@@ -27,36 +27,57 @@ function funLoad() {
 }
 
 function activeEdit(e) {
+  console.log("실행");
   e.stopPropagation();
   var currentEle = $(this);
   var value = $(this).text();
-  
-  $(currentEle).html('<input class="td_edit" type="text" value="' + value + '" />');
-  $(".td_edit").focus();
-  $(".td_edit").keyup(function (event) {
-  if (event.keyCode == 13) //공백 제거
-    $(currentEle).html($(".td_edit").val());
-  });
 
-  var dataval = parseInt($('.progress').attr("data-amount"));
+  $('.slider-text-inner').css("border", "yellow");
+
+  var dataval = parseInt($(currentEle).attr("data-amount"));
+  if(dataval == undefined || dataval == NaN) dataval = 0;
+
+  $(currentEle).html('<input class="td_edit" data-amount="0" type="text" value="' + value + '" />');
+  $(".td_edit").focus();
+
+  // 목표날짜 설정
+  //$(currentHead).html('<p>목표날짜: <input type="text" id="datepicker"></p>');
+
+  // 달성률 설정
+  $(".slider-text-inner").html('<p>목표날짜: <input type="text" id="datepicker"></p>' +
+                  '<div class="progress" data-amount="' + dataval + '">' +
+                  '달성률: <div class="amount"></div></div>' +
+                  '<button id="increase" class="button button2">+</button>' +
+                  '<button id="decrease" class="button button3">-</button>');
+
     if (dataval < 100) {
         $('.progress .amount').css("width", 100 - dataval + "%");
     }
-
-    // 달성률 체크
+    
     $('#increase').click(function () {
         modifyProgressVal(1);
     });
     $('#decrease').click(function () {
         modifyProgressVal(-1);
     });
+
     function modifyProgressVal(type) {
         dataval = parseInt($('.progress').attr("data-amount"));
         if (type == 1) dataval = Math.min(100,dataval + 10)
         else if (type == -1) dataval = Math.max(0,dataval - 10);
         $('.progress .amount').css("width", 100 - dataval + "%");
         $('.progress').attr("data-amount", dataval);
+        $(currentEle).attr("data-amount", dataval);
     }
+
+    $(".td_edit").keyup(function (event) {
+      if (event.keyCode == 13) {
+        //공백제거 후 표의 이름을 저장, 퍼센트 저장
+        $(currentEle).html($(".td_edit").val()).attr("data-amount",dataval);
+        //입력폼 삭제
+        $(".slider-text-inner").remove();
+      }
+    });
 }
 
 function activate(e) {
@@ -68,8 +89,7 @@ function activate(e) {
 
 function dismiss(e) {
   console.log("dismiss");
-  var $wrapper = $(e.currentTarget).closest('li'); 
-  console.log(e.currentTarget);
+  var $wrapper = $(e.currentTarget).closest('li');
   $wrapper
     .removeClass('active')
     .siblings().removeClass('inactive');
